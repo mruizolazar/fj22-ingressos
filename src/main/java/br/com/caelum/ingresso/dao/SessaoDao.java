2f2e7cd8d27027565fe45e1cc55935cf0a21ba06
Margarita R Olazar <margaritaruizolazar@gmail.com>
2018-01-20 11:38:04 -0200

    update project exercicio 4.4

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -27,7 +27,9 @@ public class SessaoDao {
 	}
 	
 	public List<Sessao> buscaSessoesDoFilme(Filme filme){
-		return manager.createQuery("select s from Sessao s where s.filme = :filme, Sessao.class").setParameter("filme", filme).getResultList();
+		return manager.createQuery("select s from Sessao s where s.filme = :filme", Sessao.class)
+				.setParameter("filme", filme)
+				.getResultList();
 		
 	}
 }
